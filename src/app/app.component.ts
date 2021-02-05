7a89c383221de3227dd8450a45973b12b9277c0a
khersonkavunboy <vit.nov29@gmail.com>
2021-02-05 16:51:46 +0200

    training Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+
+export interface Card {
+  title: string;
+  text: string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +15,18 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
+    toggle = true;
+
+    cards: Card[]= [
+      {title:'Card 1', text:'This 1'},
+      {title:'Card 2', text:'This 2'},
+      {title:'Card 3', text:'This 3'},
+      {title:'Card 4', text:'This 4'}
+    ];
+
+
+
+    toggleCards() : void{
+        this.toggle = !this.toggle;
+    }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Card} from '../app.component';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +8,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() card : Card;
+  @Input() index : number;
+
+
+
+  title : string = 'My card Title';
+  text : string = 'My Sample text Loreewgegjewkgweghwellgewngjewngjwngjwenjewngajngenajgejwnglajgnewjnagjanegnjawengjnewjngjaegjeajgbjkewbgjewagjbewakgj';
+  number : number = 42;
+  array : any = [1, 1, 2, 3, 4, 5];
+  obj : object = {
+    name: 'Vitek', info: { age: 25, job: 'DevJet' }
+  };
+
+  imgUrl : string = 'https://cdn.auth0.com/blog/angular5/logo.png';
+
+  disabled : boolean = false;
+
+
+  textColor : string = '';
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void{ console.log("После инициализации содержимого компонента."); }
+
+  ngOnChanges() : void {
+    console.log("ngOnChanges!!!");
   }
+
+  ngOnInit(): void {
+    console.log("ngOnInit!!!");
+  }
+
+  ngDoCheck() : void {
+    console.log("ngDoCheck!!!");
+  }
+
+  ngAfterContentChecked(): void{ console.log(" После каждой проверки содержимого компонента. Checked"); }
+  ngAfterViewInit(): void{ console.log(" После инициализации представлений компонента. ngAfterViewInit"); }
+  ngAfterViewChecked(): void{ console.log("После каждой проверки представлений компонента. ngAfterViewChecked"); }
+  ngOnDestroy(): void{ console.log(" Непосредственно перед уничтожением директивы. ngOnDestroy"); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  getInfo = () : string => 'Hello world';
+
+
+  changeTitle(): void {
+      this.card.title = 'Title has been changed';
+  }
+
+
+  inputHandler(value){
+    this.card.title = value;
+  }
+
+  changeHandler() : void {
+     console.log(this.card.title);
+  }
+
 
 }
